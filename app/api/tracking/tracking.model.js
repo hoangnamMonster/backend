de245ff1d399914286e4c41d63e7b6d38f3a38bc
hoangnamMonster <hoangnam@monsterpixel.us>
2018-04-05 11:20:36 +0700

    Semi-final commit

--- app/api/tracking/tracking.model.js
+++ app/api/tracking/tracking.model.js
@@ -20,7 +20,16 @@ var TrackingSchema = new Schema({
     delivery_date: Date,
     department_receive : String,
     price_buy : Number,
-    price_sale : Number
+    price_sale : Number,
+    time_exspect : Date,
+    cau1 : String,
+    time_cau1 : String,
+    cau2 : String,
+    time_cau2 : String,
+    cau3 : String,
+    time_cau3 : String,
+    cau4 : String,
+    time_cau4 : String
 })
 registerEvents(TrackingSchema);
 export default mongoose.model('Tracking', TrackingSchema);
